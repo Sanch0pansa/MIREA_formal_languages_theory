1dda39130e8f32f851e4b51b6a76a98137169f1b
Лебедев Александр <sashaleb2006@yandex.ru>
2024-12-01 01:11:41 +0300

    Add semantic checks to main, add error handle to main

--- course_work/main.py
+++ course_work/main.py
@@ -1,7 +1,9 @@
+from course_work.core.models.AbstractSyntaxTree import ASTException
 from course_work.core.parsers.LexicalAnalyzer import LexicalAnalyzer
 from course_work.core.parsers.SyntaxAnalyzer import SyntaxAnalyzer, SyntaxException
 import json
 
+from course_work.utils.errors_handler import handle_error
 
 lexical_table = {
     "keywords": ["true", "false", "program", "var", "end", "begin", "int", "float", "bool", "if", "else", "for", "to",
@@ -15,7 +17,8 @@ lexical_table = {
 if __name__ == "__main__":
 
     with open("./program.txt") as f:
-        text = f.read().replace("\n", " ") + " "
+        original_text = f.read()
+        text = original_text.replace("\n", " ") + " "
 
     with open("./lexical_analyzer/states.json") as f:
         states = json.load(f)
@@ -38,6 +41,9 @@ if __name__ == "__main__":
 
     try:
         p.parse()
+        p.AST.root.semantic_check()
+        print(p.AST.root.to_string())
     except SyntaxException as e:
-        print(e.message)
-        print(e.lexeme)
+        print(handle_error(e, original_text))
+    except ASTException as e:
+        print(handle_error(e, original_text))
